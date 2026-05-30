ebd409e6aa70bb44089c7089985ebb222a031cc7
Pedro Luka <pedrolukagt@gmail.com>
2026-05-30 17:28:12 -0300

    first commit

---
+++ src/test/java/antifraud/servicotransacao/ServicoTransacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package antifraud.servicotransacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicoTransacaoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
