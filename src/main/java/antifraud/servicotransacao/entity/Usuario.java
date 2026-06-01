c54c7a6c5f4f1786581c4abec2e4530aab793c7a
Pedro Luka <pedrolukagt@gmail.com>
2026-06-01 00:51:58 -0300

    feat: configs iniciais de JWT

--- src/main/java/antifraud/servicotransacao/entity/Usuario.java
+++ src/main/java/antifraud/servicotransacao/entity/Usuario.java
@@ -14,7 +14,12 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
+import java.util.List;
 import java.util.UUID;
 
 @Entity
@@ -23,7 +28,7 @@ import java.util.UUID;
 @NoArgsConstructor
 @Getter
 @Setter
-public class Usuario {
+public class Usuario implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
@@ -38,4 +43,22 @@ public class Usuario {
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private PerfilUsuario perfil;
+
+    //metodos do UserDetails
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(
+                new SimpleGrantedAuthority("ROLE_" + perfil.name())
+        );
+    }
+
+    @Override
+    public String getPassword() {
+        return senha;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
 }
---
+++ src/main/java/antifraud/servicotransacao/security/JwtService.java
@@ -0,0 +1,72 @@
+package antifraud.servicotransacao.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+
+@Service
+public class JwtService {
+
+    @Value("${jwt.secret}")
+    private String secret;
+
+    @Value("${jwt.expiration}")
+    private long JWT_EXPIRATION;
+
+    //gerar e validar token
+    public String gerarToken(UserDetails userDetails) {
+        return Jwts.builder()
+                .subject(userDetails.getUsername())
+                .issuedAt(new Date())
+                .expiration(new Date(
+                        System.currentTimeMillis() + JWT_EXPIRATION)
+                )
+                .signWith(getSigningKey())
+                .compact();
+    }
+
+    private boolean tokenExpirado(String token) {
+        return new Date().after(extrairExpiracao(token));
+    }
+
+    public boolean tokenValido(String token, UserDetails userDetails) {
+        String usernameToken = extrairUsername(token);
+        String usernameUsuario = userDetails.getUsername();
+
+        boolean usuario = usernameToken.equals(usernameUsuario);
+        boolean tokenNaoExpirou = !tokenExpirado(token);
+
+        return usuario && tokenNaoExpirou;
+    }
+
+    //metodos auxiliares para extrair informaçoes do token
+    private Claims extrairTodasClaims(String token) {
+        return Jwts.parser()
+                .verifyWith(getSigningKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+
+    public String extrairUsername(String token) {
+        Claims claims = extrairTodasClaims(token);
+        return claims.getSubject();
+    }
+
+    public Date extrairExpiracao(String token) {
+        Claims claims = extrairTodasClaims(token);
+        return claims.getExpiration();
+    }
+
+    private SecretKey getSigningKey() {
+        return Keys.hmacShaKeyFor(
+                secret.getBytes(StandardCharsets.UTF_8)
+        );
+    }
+}
