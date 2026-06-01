3705b464afc2f0813c9ed9eefd5b20b1f28a03e6
Pedro Luka <pedrolukagt@gmail.com>
2026-06-01 17:52:53 -0300

    feat: Filtro de autenticacao v1.0

---
+++ src/main/java/antifraud/servicotransacao/security/JwtAuthFilter.java
@@ -0,0 +1,58 @@
+package antifraud.servicotransacao.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.web.filter.OncePerRequestFilter;
+import java.io.IOException;
+
+@RequiredArgsConstructor
+public class JwtAuthFilter extends OncePerRequestFilter {
+
+    private final JwtService jwtService;
+    private final UsuarioDetailsService usuarioDetailsService;
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain)
+            throws ServletException, IOException {
+
+        String authHeader = request.getHeader("Authorization");
+        // verifica se o token veio na request e se inicia com Bearer
+        // se ele nao vier ou nao iniciar com Bearer, continua o fluxo normal
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        // se o token vier, extrai o username e o token (sem o Bearer)
+        String token = authHeader.substring(7);
+        String username = jwtService.extrairUsername(token);
+        UserDetails userDetails = usuarioDetailsService.loadUserByUsername(username);
+
+        // se nao estiver autenticado entra na logica de autenticar
+        if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            if (jwtService.tokenValido(token, userDetails)) {
+                // implementaçao da interface Authentication - representa a autenticaçao do usuario
+                // (Principal (usuario), Credentials (senha), Authorities (permissoes))
+                UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
+                        userDetails, null, userDetails.getAuthorities()
+                );
+
+                // contexto do Spring para autenticar o usuario - SecurityContextHolder
+                // setAuthentication recebe authToken e passa a ser autenticado
+                SecurityContextHolder.getContext().setAuthentication(authToken);
+            }
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
