a17443948887e10dc49ae6de261e9192257851d1
Pedro Luka <pedrolukagt@gmail.com>
2026-06-29 23:27:48 -0300

    feat: service para transacao

---
+++ src/main/java/antifraud/servicotransacao/service/TransacaoService.java
@@ -0,0 +1,37 @@
+package antifraud.servicotransacao.service;
+
+import antifraud.servicotransacao.dto.transacao.TransacaoRequestDTO;
+import antifraud.servicotransacao.dto.transacao.TransacaoResponseDTO;
+import antifraud.servicotransacao.entity.Transacao;
+import antifraud.servicotransacao.messaging.TransacaoPublisher;
+import antifraud.servicotransacao.repository.TransacaoRepository;
+import antifraud.servicotransacao.util.TransacaoMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class TransacaoService {
+
+    private final TransacaoRepository transacaoRepository;
+    private final TransacaoPublisher transacaoPublisher;
+
+    // garante rollback caso ocorra qualquer exceçao durante o processamento,
+    // incluindo falhas ao publicar o evento no RabbitMQ
+    @Transactional
+    public TransacaoResponseDTO registrarTransacao(TransacaoRequestDTO requestDTO) {
+        Transacao transacao = TransacaoMapper.toEntity(requestDTO);
+
+        Transacao transacaoSalva = transacaoRepository.save(transacao);
+        transacaoPublisher.publicarTransacao(TransacaoMapper.toEventoDTO(transacaoSalva));
+
+        log.info("Transação registrada: ID={}, Conta={}",
+                transacaoSalva.getId(),
+                transacaoSalva.getContaId());
+
+        return TransacaoMapper.toResponseDTO(transacaoSalva);
+    }
+}
