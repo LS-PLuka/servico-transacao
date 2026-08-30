5d4c68f4a07fd15d8df82d1a853d454326c9917c
Pedro Luka <pedrolukagt@gmail.com>
2026-08-30 20:50:21 -0300

    feat: lista transacoes de uma conta - service

--- src/main/java/antifraud/servicotransacao/repository/TransacaoRepository.java
+++ src/main/java/antifraud/servicotransacao/repository/TransacaoRepository.java
@@ -2,6 +2,8 @@ package antifraud.servicotransacao.repository;
 
 import antifraud.servicotransacao.entity.Transacao;
 import antifraud.servicotransacao.enums.StatusTransacao;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,7 +13,7 @@ import java.util.UUID;
 @Repository
 public interface TransacaoRepository extends JpaRepository<Transacao, UUID> {
 
-    List<Transacao> findByContaId(UUID contaId);
+    Page<Transacao> findByContaId(UUID contaId, Pageable pageable);
 
     List<Transacao> findByContaIdAndStatus(UUID contaId, StatusTransacao status);
 }
--- src/main/java/antifraud/servicotransacao/service/TransacaoService.java
+++ src/main/java/antifraud/servicotransacao/service/TransacaoService.java
@@ -3,15 +3,27 @@ package antifraud.servicotransacao.service;
 import antifraud.servicotransacao.dto.transacao.TransacaoRequestDTO;
 import antifraud.servicotransacao.dto.transacao.TransacaoResponseDTO;
 import antifraud.servicotransacao.entity.Transacao;
+import antifraud.servicotransacao.entity.Usuario;
+import antifraud.servicotransacao.exception.AcessoNegadoException;
 import antifraud.servicotransacao.exception.TransacaoNaoEncontradaException;
+import antifraud.servicotransacao.exception.UsuarioNaoEncontradoException;
 import antifraud.servicotransacao.messaging.TransacaoPublisher;
 import antifraud.servicotransacao.repository.TransacaoRepository;
+import antifraud.servicotransacao.repository.UsuarioRepository;
+import antifraud.servicotransacao.util.PaginaResponseDTO;
+import antifraud.servicotransacao.util.PaginaResponseMapper;
 import antifraud.servicotransacao.util.TransacaoMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.UUID;
 
 import static antifraud.servicotransacao.util.TransacaoMapper.toResponseDTO;
@@ -23,6 +35,7 @@ public class TransacaoService {
 
     private final TransacaoRepository transacaoRepository;
     private final TransacaoPublisher transacaoPublisher;
+    private final UsuarioRepository usuarioRepository;
 
     // garante rollback caso ocorra qualquer exceçao durante o processamento,
     // incluindo falhas ao publicar o evento no RabbitMQ
@@ -47,5 +60,32 @@ public class TransacaoService {
 
         log.info("Transação encontrada: ID={}, Conta={}", transacao.getId(), transacao.getContaId());
         return toResponseDTO(transacao);
+    }
+
+    @Transactional(readOnly = true)
+    public PaginaResponseDTO<TransacaoResponseDTO> listarTransacoesDeUmaConta(UUID contaId, String token, int pagina) {
+        // busca usuario autenticado
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        Usuario usuarioLogado = (Usuario) authentication.getPrincipal();
+
+        Usuario usuario = usuarioRepository.findById(contaId)
+                .orElseThrow(() -> new UsuarioNaoEncontradoException("Usuário não encontrado"));
+
+        // verifica tipo de perfil
+        boolean isAdmin = usuarioLogado.getPerfil().name().equals("ADMIN");
+        boolean isContaDoUsuario = usuario.getId().equals(usuarioLogado.getId());
+
+        // se o usuario logado nao for ADMIN e contaId nao for dele,
+        // ele nao tem permissao para acessar
+        if (contaId != usuarioLogado.getId() && !isAdmin) {
+            log.warn("Tentativa de acesso não autorizado às transações de outra conta: Conta={}, UsuarioLogado={}", contaId, usuarioLogado.getId());
+            throw new AcessoNegadoException("Você não tem permissão para acessar as transações de outra conta");
+        }
+
+        Pageable pageable = PageRequest.of(pagina, 10);
+        Page<Transacao> transacoesPage = transacaoRepository.findByContaId(contaId, pageable);
+
+        log.info("Listando transações para a conta: Conta={}, Pagina={}, TotalTransacoes={}", contaId, pagina, transacoesPage.getTotalElements());
+        return PaginaResponseMapper.fromPage(transacoesPage.map(transacao -> toResponseDTO(transacao)));
     }
 }
